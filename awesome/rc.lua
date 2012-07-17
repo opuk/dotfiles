dac0f9d71c784a8c759348e747fb4effc8e18778
Johan Swensson <kupo@kupo.se>
2012-07-17 13:45:27 +0200

    added volume control

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -240,6 +240,11 @@ globalkeys = awful.util.table.join(
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
+    -- Thinkpad volume control
+    awful.key({ }, "XF86AudioLowerVolume", function () awful.util.spawn("amixer -q -c 0 sset Master 2dB-") end),
+    awful.key({ }, "XF86AudioRaiseVolume", function () awful.util.spawn("amixer -q -c 0 sset Master 2dB+") end),
+    awful.key({ }, "XF86AudioMute", function () awful.util.spawn("amixer -q sset Master toggle") end),
+
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
 
