6f9da5d4363443f1f8969df72419c82c7082b1a6
Johan Swensson <kupo@kupo.se>
2015-05-25 21:59:56 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ pushd .i3
 ln -s ../dotfiles/.i3/config config
 popd
 ln -s dotfiles/i3status.conf .i3status.conf
+sudo cp dotfiles/misc/10-serverflags.conf dotfiles/misc/20-thinkpad-trackpoint.conf /etc/X11/xorg.conf.d
